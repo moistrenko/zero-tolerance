c84eae224fe6b80426bba7bb3c3fedb7ba41c9ab
Anatoly Moistrenko <moistrenko@mail.ru>
2024-10-16 16:21:02 +0300

    feat: added player jump

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { RapierRigidBody, RigidBody } from '@react-three/rapier';
+import * as RAPIER from '@dimforge/rapier3d-compat';
+import { RapierRigidBody, useRapier, RigidBody } from '@react-three/rapier';
 import { MutableRefObject, RefObject, useRef } from 'react';
 import { usePersonControls } from 'hooks/hooks';
 import { useFrame } from '@react-three/fiber';
@@ -12,7 +13,9 @@ const sideVector = new THREE.Vector3();
 export const Player = () => {
   const playerRef: RefObject<RapierRigidBody> | MutableRefObject<RapierRigidBody> = useRef(null);
 
-  const { forward, backward, left, right } = usePersonControls();
+  const { forward, backward, left, right, jump } = usePersonControls();
+
+  const rapier = useRapier();
 
   useFrame(() => {
     if (!playerRef.current) return;
@@ -25,11 +28,27 @@ export const Player = () => {
 
     playerRef.current.wakeUp();
     playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z }, true);
+
+    const world = rapier.world;
+    const ray = world.castRay(
+      new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }),
+      0,
+      false
+    );
+
+    // const grounded = ray && ray.collider && Math.abs(ray.toi) <= 0.75;
+    const grounded = ray && ray.collider;
+    if (jump && grounded) doJump();
   });
+
+  const doJump = () => {
+    if (!playerRef.current) return;
+    playerRef.current.setLinvel({ x: 0, y: 8, z: 0 }, false);
+  };
 
   return (
     <>
-      <RigidBody position={[0, 1, -2]} ref={playerRef}>
+      <RigidBody position={[0, 1, -2]} mass={1} ref={playerRef} lockRotations>
         <mesh>
           <capsuleGeometry args={[0.5, 0.5]} />
         </mesh>
